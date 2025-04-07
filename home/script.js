589354d63b367d412e2fac9d7aae7e1ef343c098
Caetano-Uriel <urielferrazc@gmail.com>
2025-04-06 21:27:20 -0300

    Add files via upload

--- home/script.js
+++ home/script.js
@@ -1,8 +1,3 @@
-document.getElementById("btn-personagens").addEventListener("click", function (e) {
-  e.preventDefault(); 
-  document.body.classList.add("fade-out"); 
-
-  setTimeout(() => {
-    window.location.href = "../personagens//index.html"; 
-  }, 600); // Aguarda a duração da transição antes de mudar de página
-});
+function telapersonagem() {
+  window.location.href = "../index.html";
+}
